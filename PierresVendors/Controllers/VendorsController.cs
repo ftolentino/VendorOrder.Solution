837b28bdc791dd9e72f9aa296fe680d9b251daef
ftolentino <filmer.tolentino@gmail.com>
2022-07-24 14:28:49 -0700

    Update Order arguments to include new price property

--- PierresVendors/Controllers/VendorsController.cs
+++ PierresVendors/Controllers/VendorsController.cs
@@ -39,11 +39,11 @@ namespace PierresVendors.Controllers
     }
     // this one creates new orders within a given vendor, not new vendors;
     [HttpPost("/vendors/{vendorId}/orders")]
-    public ActionResult New(int vendorId, string orderDescription)
+    public ActionResult New(int vendorId, string orderDescription, double price)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
       Vendor foundVendor = Vendor.Find(vendorId);
-      Order newOrder = new Order(orderDescription);
+      Order newOrder = new Order(orderDescription, price);
       foundVendor.AddOrder(newOrder);
       List<Order> vendorOrders = foundVendor.Orders;
       model.Add("orders", vendorOrders);
