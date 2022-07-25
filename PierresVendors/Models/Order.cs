7955f98269a45d0c5261a2ce906d693a8d2eef71
ftolentino <filmer.tolentino@gmail.com>
2022-07-24 17:23:57 -0700

    Add Title and Date properties

--- PierresVendors/Models/Order.cs
+++ PierresVendors/Models/Order.cs
@@ -1,16 +1,23 @@
 using System.Collections.Generic;
+using System;
 
 namespace PierresVendors.Models
 {
   public class Order
   {
+    public string Title { get; set; }
     public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public double Price { get; set; }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> {};
 
-    public Order(string description)
+    public Order(string description, double price, string title, DateTime date)
     {
+      Title = title;
       Description = description;
+      Date = date;
+      Price = price;
       _instances.Add(this);
       Id = _instances.Count;
     }
